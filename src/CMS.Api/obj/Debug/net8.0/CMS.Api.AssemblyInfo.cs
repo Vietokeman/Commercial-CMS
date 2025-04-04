43ed1e06fa26bb53dd15f974ea7dbb4b17662445
Nguyễn Việt <vietbmt19@gmail.com>
2025-04-04 22:29:19 +0700

    filter folder

--- src/CMS.Api/obj/Debug/net8.0/CMS.Api.AssemblyInfo.cs
+++ src/CMS.Api/obj/Debug/net8.0/CMS.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c11a648ccfb702a3c092e1c1ed9d222b2e040c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8143cdbd4e30f25e6bcb7929757a8f16ac39a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
