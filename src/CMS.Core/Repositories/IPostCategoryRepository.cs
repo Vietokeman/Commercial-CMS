ad2d624a2851e7e36e3fc11e9fcb845ac39b18e7
Nguyễn Việt <vietbmt19@gmail.com>
2025-06-12 02:26:22 +0700

    add page for paging and post

--- src/CMS.Core/Repositories/IPostCategoryRepository.cs
+++ src/CMS.Core/Repositories/IPostCategoryRepository.cs
@@ -7,6 +7,8 @@ namespace CMS.Core.Repositories
 {
     public interface IPostCategoryRepository : IRepository<PostCategory, Guid>
     {
+        Task<PostCategoryDto> GetBySlug(string slug);
+
         Task<PageResult<PostCategoryDto>> GetPostCategorysPagingAsync(string? keyword, int PageIndex = 1, int pageSize = 10);
     }
 }
