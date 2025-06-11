5aa0a1e307b84177431747e26a88e38af3cbff33
Nguyễn Việt <vietbmt19@gmail.com>
2025-06-11 23:48:49 +0700

    add environment net core and user claim principal

--- CMS.WebApp/Program.cs
+++ CMS.WebApp/Program.cs
@@ -1,8 +1,35 @@
+using CMS.Core.ConfigOptions;
+using CMS.Core.Domain.Identity;
+using CMS.Data;
+using CMS.WebApp.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//system config
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+//config environment for development and production
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+//setup custom 
+builder.Services.Configure<SystemConfig>(configuration.GetSection("SystemConfig"));
+
+//set up DB for web app
+builder.Services.AddDbContext<CMSDbContext>(o => o.UseSqlServer(connectionString));
+builder.Services.AddIdentity<AppUser, AppRole>(o => o.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<CMSDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<AppUser>, CustomClaimsPrincipalFactory>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
